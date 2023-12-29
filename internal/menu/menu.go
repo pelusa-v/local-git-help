07939910d20673c3e0edaf7a394c5e0199dcd034
pelusa-v <razor0164@gmail.com>
2023-12-28 23:40:14 -0500

    Menu done

--- internal/menu/menu.go
+++ internal/menu/menu.go
@@ -23,7 +23,6 @@ func MenuPrinter() {
 	ShowMenu(option, limit)
 
 	for {
-		clearScreen()
 		key_value, key, err := keyboard.GetKey()
 		if err != nil {
 			log.Fatal(err)
@@ -37,6 +36,8 @@ func MenuPrinter() {
 			break
 		}
 
+		// ShowMenu(option, limit)
+		clearScreen(limit + 2)
 		ShowMenu(option, limit)
 	}
 }
@@ -53,6 +54,55 @@ func ShowMenu(option int, limit int) {
 	fmt.Println("===================================")
 }
 
-func clearScreen() {
-	fmt.Print("\033[H\033[2J") // ANSI escape codes to clear the screen
+// const escape = "\033"
+// const clearLine = escape + "[2K" // ANSI escape code to clear the current line
+
+// func moveCursorUp(lines int) string {
+// 	return fmt.Sprintf("%s[%dA", escape, lines)
+// }
+
+// func clearScreen(lines int) {
+// 	cmd := exec.Command("clear")
+// 	cmd.Stdout = os.Stdout
+// 	cmd.Run()
+
+// 	// fmt.Print("\033[H")
+
+// 	fmt.Print(moveCursorUp(lines))
+// 	fmt.Print(clearLine)
+// }
+
+const escape = "\033"
+
+// ANSI escape code to clear the entire screen
+const clearScreenStr = escape + "[2J"
+
+// ANSI escape code to move the cursor to the beginning of the line
+const moveToBeginningOfLine = escape + "[0G"
+
+//
+//
+//
+//
+//
+//
+//
+
+// ANSI escape code to move the cursor up one line
+const moveCursorUp1 = escape + "[F"
+
+// ANSI escape code to clear the entire line
+const clearLine = escape + "[2K"
+
+// ANSI escape code to move the cursor up by a specified number of lines
+func moveCursorUp(lines int) string {
+	return fmt.Sprintf("%s[%dA", escape, lines)
+}
+
+func clearScreen(lines int) {
+	for i := 0; i < lines; i++ {
+		fmt.Print(moveCursorUp1 + clearLine)
+	}
+	// fmt.Print(clearScreenStr)
+	// fmt.Print(moveCursorUp(lines))
 }
